0f9bbbdd964529084fff7599f3557cea30f241d3
SamyukdhaDhamotharan <131771291+SamyukdhaDhamotharan@users.noreply.github.com>
2024-04-03 19:32:32 +0530

    script.js

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById('display');
+
+function Display(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function del(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
